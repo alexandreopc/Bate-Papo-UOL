2725d94c904dc6ba379a735ef0655f20e9c373b6
Alexandre Costa <alexandreodpc@gmail.com>
2022-02-14 14:07:43 -0300

    correcao3

--- scripts/script.js
+++ scripts/script.js
@@ -41,6 +41,14 @@ function buscarMsg() {
                     <p>(${mensagens[i].time})<strong> ${mensagens[i].from}</strong> ${mensagens[i].text}</p>
                 `
             }
+            // private_message
+            if(mensagens[i].type === "private_message" && mensagens[i].to === nome) {
+                let mensagem = document.querySelector(".container"); 
+                mensagem.innerHTML += `
+                <div class="mensagem reservada" data-identifier="message">
+                    <p>(${mensagens[i].time})<strong> ${mensagens[i].from}</strong> reservadamente para <strong>${mensagens[i].to}</strong>: ${mensagens[i].text}</p>
+                `
+            }
         }
     })
     .catch(function(erro) {
